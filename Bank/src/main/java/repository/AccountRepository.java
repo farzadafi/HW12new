5fdbb72040c78436494bf579d1b1029e69904868
farzad <farzadafi40@gmail.com>
2022-03-01 09:36:28 +0330

    add find method

--- Bank/src/main/java/repository/AccountRepository.java
+++ Bank/src/main/java/repository/AccountRepository.java
@@ -2,8 +2,10 @@ package repository;
 
 import entity.Account;
 import entity.enumoration.TypeAccount;
+import org.hibernate.SQLQuery;
 import org.hibernate.SessionFactory;
 
+import javax.persistence.Query;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -33,31 +35,24 @@ public class AccountRepository implements Repository<Account> {
         }
     }
 
-    @Override
-    public int find(String accountNumber) throws SQLException {
-        String find = "SELECT * FROM Account WHERE accountnumber = ? ";
-        PreparedStatement preparedStatement = connection.prepareStatement(find);
-        preparedStatement.setString(1,accountNumber);
-        ResultSet resultSet = preparedStatement.executeQuery();
-        if(resultSet.next())
-            return 1;
-        else
-            return 0;
+    public int find(String accountNumber) {
+        try (var session = sessionFactory.openSession()) {
+            var query = session.createSQLQuery("SELECT * FROM Account WHERE accountnumber = :accountNumber");
+            query.addEntity(Account.class);
+            query.setParameter("accountNumber",accountNumber);
+            List account = query.list();
+
+            if(account.size() == 0 ) {
+                System.out.println("This account is not found!");
+                return 0;
+            }
+            else {
+                System.out.println("This account is found!");
+                return 1;
+            }
+        }
     }
 
-    public int showAccount(String nationalId) throws SQLException {
-        String show = "SELECT * FROM Account WHERE nationalId = ? ";
-        PreparedStatement preparedStatement = connection.prepareStatement(show);
-        preparedStatement.setString(1,nationalId);
-        ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                if ((resultSet.getString("TypeAccount").equals("ACTIVE"))) {
-                    System.out.print(resultSet.getInt("id") + ": ");
-                    System.out.println(resultSet.getString("accountnumber"));
-                }
-            }
-            return 1;
-    }
 
     public String returnAccountNumber(int id) throws SQLException {
         String returnNumber = "SELECT * FROM Account WHERE id = ? ";
