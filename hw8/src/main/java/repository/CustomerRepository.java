d8118f019c40108ad078860d5fc645f21da2f860
farzad <farzadafi40@gmail.com>
2022-03-02 13:56:53 +0330

    fix findAll method

--- hw8/src/main/java/repository/CustomerRepository.java
+++ hw8/src/main/java/repository/CustomerRepository.java
@@ -1,20 +1,18 @@
 package repository;
 
 import entity.Customer;
-import entity.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.NativeQuery;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerRepository implements Repository<Customer> {
-    private Connection connection;
-    private final SessionFactory sessionFactory = SessionFactorySingleton.getInstance();
+    private final SessionFactory sessionFactory;
+
+    public CustomerRepository(){
+        sessionFactory = SessionFactorySingleton.getInstance();
+    }
 
     public int add(Customer customer){
         try (var session = sessionFactory.openSession()) {
@@ -29,40 +27,6 @@ public class CustomerRepository implements Repository<Customer> {
             }
         }
     }
-
-
-    public CustomerRepository() {
-        try {
-            connection = Singleton.getInstance().getConnection();
-        }catch (SQLException | ClassNotFoundException exception){
-            System.out.println(exception.getMessage());
-        }
-    }
-
-
-    /*
-    @Override
-    public List<Customer> findAll() throws SQLException {
-            String findAll = "SELECT * FROM UserTable" ;
-            PreparedStatement preparedStatement = connection.prepareStatement(findAll);
-            List<Customer> customerList = new ArrayList<>();
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.isBeforeFirst()) {
-                while (resultSet.next()) {
-                    Customer customer = new Customer();
-                    customer.setId(resultSet.getInt("id"));
-                    customer.setFullName(resultSet.getString("fullName"));
-                    customer.setNationalId(resultSet.getString("nationalId"));
-                    customer.setPassword(resultSet.getString("password"));
-                    customer.setBalance(resultSet.getDouble("balance"));
-                    customer.setAddress(resultSet.getString("address"));
-                    customerList.add(customer);
-                }
-                return customerList;
-            } else
-                return null;
-    }
-     */
 
     @Override
     public List<Customer> findAll(){
